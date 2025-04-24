66411fba14c38577a6da17d8418a452aea055744
IKEDA Yasunobu <ics-ikeda@users.noreply.github.com>
2025-04-24 20:47:22 +0900

    prettier format eslint.config.mjs

--- sample-prettier-eslint/eslint.config.mjs
+++ sample-prettier-eslint/eslint.config.mjs
@@ -1,6 +1,6 @@
-import neostandard from 'neostandard';
+import neostandard from "neostandard";
 
 export default neostandard({
-    noStyle: true,
-    env: ['browser']
-})
+  noStyle: true,
+  env: ["browser"],
+});
